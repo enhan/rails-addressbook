7bd0c011d492881624f610d3cbd6b9dc541a49b0
Emmanuel Nhan <nhanmanu@gmail.com>
2012-10-14 11:37:58 +0200

    Create contact form and show contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+	  @contact = Contact.new
   end
+  
+  def show
+	  @contact = Contact.find(params[:id])
+  end
+  
+  def create
+	  @contact = Contact.new(params[:contact])
+	  if @contact.save
+		  redirect_to @contact
+	  else
+		  render 'new'
+	  end
+  end
+  
 end
